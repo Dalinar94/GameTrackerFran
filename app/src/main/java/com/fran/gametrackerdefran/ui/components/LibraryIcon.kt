32343c3c0153b2900ee4c573c7b1d22632282a55
Dalinar94 <frandavowork@gmail.com>
2026-07-26 18:08:01 +0200

    main:iconos reales de plataformas

--- app/src/main/java/com/fran/gametrackerdefran/ui/components/LibraryIcon.kt
+++ app/src/main/java/com/fran/gametrackerdefran/ui/components/LibraryIcon.kt
@@ -1,44 +1,38 @@
 package com.fran.gametrackerdefran.ui.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Computer
-import androidx.compose.material.icons.filled.SportsEsports
-import androidx.compose.material.icons.filled.VideogameAsset
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
-
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.fran.gametrackerdefran.R
+import androidx.compose.ui.graphics.Color
 @Composable
 fun LibraryIcon(
     library: String
 ) {
 
-    val icon: ImageVector = when (library) {
+    val icon = when (library) {
+        "Steam" -> R.drawable.ic_steam
+        "Steam Deck" -> R.drawable.ic_steam_deck
+        "Epic Games" -> R.drawable.ic_epic_games
+        "EA App" -> R.drawable.ic_ea
+        "Battle.net" -> R.drawable.ic_battlenet
+        "Ubisoft Connect" -> R.drawable.ic_ubisoft
+        "GOG" -> R.drawable.ic_gog
+        "Xbox App" -> R.drawable.ic_xbox
+        "Nintendo Switch" -> R.drawable.ic_nintendo_switch2
 
-        "Steam" -> Icons.Default.Computer
 
-        "Epic Games" -> Icons.Default.VideogameAsset
-
-        "EA App" -> Icons.Default.SportsEsports
-
-        "Battle.net" -> Icons.Default.SportsEsports
-
-        "Ubisoft Connect" -> Icons.Default.SportsEsports
-
-        "GOG" -> Icons.Default.Computer
-
-        "Xbox App" -> Icons.Default.SportsEsports
-
-        "Nintendo Switch" -> Icons.Default.VideogameAsset
-
-        "Rockstar Games Launcher" -> Icons.Default.SportsEsports
-
-        else -> Icons.Default.SportsEsports
+        else -> R.drawable.ic_steam
     }
 
     Icon(
-        imageVector = icon,
-        contentDescription = library
+        painter = painterResource(id = icon),
+        contentDescription = library,
+        modifier = Modifier.size(22.dp),
+        tint = Color.Unspecified
     )
 
 }
